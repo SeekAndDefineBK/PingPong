b7569e0db5b7760ab35dff69dac5844e042107c9
Brett Koster <skdfmanagement@icloud.com>
2022-07-03 11:05:27 -0500

    Initial Commit

---
+++ PingPong/PingPongApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingPongApp.swift
+//  PingPong
+//
+//  Created by Brett Koster on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct PingPongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
